7f694c1de5e3439be085650cc1526cc211991b11
gabriella368 <72966648+gabriella368@users.noreply.github.com>
2024-02-03 18:28:51 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/mytasks/MainActivity.java
+++ app/src/main/java/com/example/mytasks/MainActivity.java
@@ -1,47 +1,68 @@
-package com.example.mytasks;
+package com.example.todolistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> items;
+    private ListView list;
+    private Button button;
+    private ArrayAdapter<String> itemsAdapter;
 
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
-        user = auth.getCurrentUser();
-        if (user == null){
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            startActivity(intent);
-             finish();
-        }
-        else {
-            textView.setText(user.getEmail());
-        }
+
+        list = findViewById(R.id.list);
+        button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-               finish();
+                additem(view);
             }
         });
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        list.setAdapter(itemsAdapter);
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return remove(position);
+            }
+        });
+    }
+
+    private boolean remove(int position){
+        Context context = getApplicationContext();
+        Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+        items.remove(position);
+        itemsAdapter.notifyDataSetChanged();
+        return true;
+    }
+
+    private void additem(View view) {
+        EditText input = findViewById(R.id.edit_text);
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(""))){
+            itemsAdapter.add(itemText);
+            input.setText("");
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Please Enter Text", Toast.LENGTH_LONG).show();
+        }
     }
 }
